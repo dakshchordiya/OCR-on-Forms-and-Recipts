1191c1144847509834da91bc3ae56820af6837a9
Daksh Chordiya <dakshchordiya007@gmail.com>
2020-11-29 16:42:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+import pytesseract
+import os
+
+pytesseract.pytesseract.tesseract_cmd = 'D:\\daksh\\OpenCV Examples\\Projects\\Tesseract OCR text Detection\\Tesseract-OCR\\tesseract.exe'
